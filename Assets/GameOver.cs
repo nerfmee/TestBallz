ac4fe7c98531a97204a919adfe1db8fe0c132e83
nerfmee <41577753+nerfmee@users.noreply.github.com>
2020-06-03 18:52:40 +0300

    AddGameOver
    GameOver line and gameOverPanel

---
+++ Assets/GameOver.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    private bool _gameOver;
+    [SerializeField] private GameObject GameOverPanel;
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        _gameOver = true;
+        GameOverPanel.SetActive(true);
+    }
+}
